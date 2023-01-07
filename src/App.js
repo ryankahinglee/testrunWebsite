22039251c30bbad73042cd4d5d7714d2d4011338
ryankahinglee <ryankahinglee@gmail.com>
2023-01-07 12:21:06 +1100

    mobile debug

--- src/App.js
+++ src/App.js
@@ -23,6 +23,27 @@ function App() {
 
   const [section, setSection] = React.useState('Home');
 
+  // Reference to an output container, use 'pre' styling for JSON output
+  var output = document.createElement('pre');
+  document.body.appendChild(output);
+
+  // Reference to native method(s)
+  var oldLog = console.log;
+
+  console.log = function( ...items ) {
+
+      // Call native method first
+      oldLog.apply(this,items);
+
+      // Use JSON to transform objects, all others display normally
+      items.forEach( (item,i)=>{
+          items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
+      });
+      output.innerHTML += items.join(' ') + '<br />';
+
+  };
+
+
   return (
     <div>    
       {viewportSize >= 1470 && (
